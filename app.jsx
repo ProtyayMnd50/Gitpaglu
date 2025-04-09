4cf76a0fd3bc2f322caddfe56292fc310d54801a
Protyay Mondal <protyaymondal32@gmail.com>
2025-04-09 13:13:22 +0530

    made changes to app.jsx

--- app.jsx
+++ app.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 
 const app = () => {
   const [cnt, setcnt] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const col = toggle === false ? "green" : "red";
 
   return (
     <>
+      <h1>Counter</h1>
       <div>count is: {cnt}</div>
       <button onClick={() => setcnt(cnt + 1)}>+</button>
       <button onClick={() => setcnt(cnt - 1)}>-</button>
+
+      <h1>Toggler</h1>
+      <button style={{ color: col }}>Button color</button>
+      <button onClick={() => setToggle(!toggle)}>Toggle button</button>
     </>
   );
 };
